8be2ddecead6fd569286b1b6d6cfe79265fd2a68
e10withadot <e10withadot@gmail.com>
2025-05-22 18:08:24 +0300

    added basic player header

---
+++ player.hpp
@@ -0,0 +1,12 @@
+namespace coup {
+	class Player {
+		void gather();
+		void tax();
+		void invest();
+		void bribe();
+		int coins();
+		void arrest(Player target);
+		void sanction(Player target);
+		void coup(Player target);
+	};
+}
